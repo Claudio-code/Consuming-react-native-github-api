8f0a253f250e10da692f3c5677067f61025c80c9
claudio-code <suporte2424@outlook.com>
2020-01-26 05:54:16 -0300

    configured routes and pages

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { KeyboardAvoidingView, Text } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import Users from './pages/Users';
+
+const mainStackNavigation = createStackNavigator({
+    Main: {
+        screen: Main,
+        navigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#009688'
+            },
+            headerTitleAlign: 'center',
+            headerTitle: 'Dashboard',
+            headerTintColor: '#FFF',
+        
+        })
+    },
+    Users: {
+        screen: Users,
+        navigationOptions: ({navigation}) => ({
+            headerTitleAlign: 'center',
+            headerTitle: 'Usuarios',
+            headerTintColor: '#FFF',
+            headerStyle: {
+                backgroundColor: '#009688'
+            }
+        })
+    }
+});
+
+
+const app = createSwitchNavigator({
+    mainStackNavigation: { screen: mainStackNavigation }
+});
+
+const Routes = createAppContainer(app);
+
+export default Routes;
