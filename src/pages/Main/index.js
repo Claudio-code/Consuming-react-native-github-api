087f9422a8bc79c99bc60c17922a0c9e58edef62
claudio-code <suporte2424@outlook.com>
2020-01-26 21:12:17 -0300

    styles in dashboard is set

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
   return (
-    <View />
+    <Container>
+      <Form>
+        <Input 
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuario"
+          placeholderTextColor="#757575"
+        />
+        <SubmitButton>
+          <Icon 
+            name="add"
+            size={20}
+            color="#FFF"
+          />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,5 +1,32 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
-  
+  flex: 1;
+  padding: 30px;
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Input = styled.TextInput`
+  flex: 1;
+  height: 40px;
+  background: #e0e0e0;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #e0e0e0;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #009688;
+  border-radius: 4px;
+  margin-left: 12px;
+  padding: 0 12px;
 `;
